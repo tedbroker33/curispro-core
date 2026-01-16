d40b22dd5db80951e36b67e703dd5ff516ed4698
tedBroker33 <Twenecki@myhst.com>
2026-01-16 13:00:47 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    domains: ['curispro.com'], // Add your domain later
+  },
 }
 
 module.exports = nextConfig

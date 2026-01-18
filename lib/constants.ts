f72571cb6ffc8bc7cadc92ab8af7190a86e79742
tedBroker33 <Twenecki@myhst.com>
2026-01-17 20:37:04 -0600

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,26 +3,15 @@ export const BETA_PRICE = 61;
 export const MASTER_CAP = 33;    
 
 export const ELITE_AGENTS = {
-  'cjennings@myhst.com': { 
-    name: 'Corey Jennings', 
-    trial: 88, 
-    quote: "The measure of intelligence is the ability to change. Your loyalty is your power." 
-  },
-  'lwilds@myhst.com': { 
-    name: 'Lauren Wilds', 
-    trial: 88, 
-    quote: "You are a warrior of light; your devotion is the frequency that heals the team." 
-  },
-  'jdavern@myhst.com': { 
-    name: 'Joe Davern', 
-    trial: 1095, 
-    quote: "Partnership is the fuel that allows common people to attain uncommon results. This legacy is ours." 
-  }
+  'cjennings@myhst.com': { name: 'Corey Jennings', trial: 88, quote: "Intelligence is the ability to change. Your loyalty is your power." },
+  'lwilds@myhst.com': { name: 'Lauren Wilds', trial: 88, quote: "You are a warrior of light; your devotion heals the team." },
+  'jdavern@myhst.com': { name: 'Joe Davern', trial: 1095, quote: "Partnership is the fuel for uncommon results. This legacy is ours." },
+  'twenecki@myhst.com': { name: 'Ted Wenecki', trial: 10000, quote: "The Architect. The Master Teacher. The system is yours." } // ADDED YOU HERE
 };
 
 export const BETA_LIST = [
   'vneely@myhst.com', 'raden@myhst.com', 'lgodoipiresgunnrussell@myhst.com',
   'ctucker@myhst.com', 'thackney@myhst.com', 'wwoerner@myhst.com',
   'mcountryman@myhst.com', 'alakejr@myhst.com', 'bduncan@myhst.com',
-  'candacep90@yahoo.com'
+  'candacep90@yahoo.com', 'twenecki@myhst.com' // ADDED YOU HERE
 ];
